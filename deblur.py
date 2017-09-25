e836d4ef65cd6db0a2d43a21fc18fce732f788c5
Marat Nabiev <nma2207@gmail.com>
2017-09-25 18:53:06 +0300

    try to implement l-r method

--- deblur.py
+++ deblur.py
@@ -1,8 +1,11 @@
 #coding: utf-8
 
 import numpy as np
+import filters
+import scipy.signal
 
-def inverse_filter(g,h):
+
+def inverseFilter(g,h):
     width_g = g.shape[0]
     height_g = g.shape[1]
     width_h = h.shape[0]
@@ -18,5 +21,26 @@ def inverse_filter(g,h):
     f = np.real(f)
     f = f[0:width_g, 0:height_g]
     return f
+
+def blindLucyRichardsonMethod(img, N, M, K, initKernel = 'gauss'):
+    kernel = None
+    if initKernel=='gauss':
+        kernel = filters.getGaussian(1, img.shape)
+
+    f = np.copy(img)
+    for k in range(K):
+        print(k)
+        for n in range(N):
+            print('n--',n)
+            div = img / (scipy.signal.fftconvolve( f, kernel, mode = 'same'))
+            kernel =  kernel * (scipy.signal.correlate(f, div, mode = 'same', method = 'fft'))
+        kernel /= np.sum(kernel)
+        for m in range(M):
+            print('m--',m)
+            div = img / (scipy.signal.fftconvolve(kernel, f, mode = 'same'))
+            f = f*(scipy.signal.correlate(kernel, div, mode = 'same', method='fft'))
+    return f
+
+
 
 
